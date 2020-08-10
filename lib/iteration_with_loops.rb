81db03847a1a17166a9223165c2970bca75cbbf0
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-10 16:21:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count do
       if element_index.is_a? String
-        joined.concat(src[row_index][element_index])
+        joined.concat(src[row_index][element_index] + " ")
       end 
     element_index += 1 
     end
